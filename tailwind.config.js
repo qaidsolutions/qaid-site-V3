14a74df3673b8155ec27e2632d765cd3a647aee7
qaidsolutions <qaid.solutions@gmail.com>
2025-11-07 15:41:53 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,2 @@
+
+module.exports = { content: ["./pages/**/*.{js,jsx}","./components/**/*.{js,jsx}"], theme:{ extend:{ colors:{ qaidTeal:"#00f0c2", darkBg:"#070707", card:"#0f1112" } } }, plugins:[] }
